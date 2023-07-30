4719b027ed90190500e87dec9f2a4f5efe02aa1f
M4RT1N <mjlynch35@yahoo.com>
2023-07-29 20:41:45 -0500

    updates

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,17 @@ const { User } = require("../models");
 // GET all users for homepage
 router.get("/", async (req, res) => {
   console.log("Hello123");
+  try {
+    // Assuming you have a method to fetch all users from your User model
+    const users = await User.findAll();
+
+    // Send the users as a JSON response
+    res.json(users);
+  } catch (error) {
+    // Handle any errors that might occur during the database query or processing
+    console.error("Error fetching users:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 });
 
 module.exports = router;
