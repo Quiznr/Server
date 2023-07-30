8e5f012a3df7b33fb719782c236f43b65e325cf4
M4RT1N <mjlynch35@yahoo.com>
2023-07-29 19:54:48 -0500

    updates

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,17 +2,8 @@ const router = require("express").Router();
 const { User } = require("../models");
 
 // GET all users for homepage
-router.get("/", async (req, res) => {
+router.get("/home", async (req, res) => {
   console.log("Hello123");
-  try {
-    const dbUserData = await User.findAll({
-      attributes: { exclude: ["password"] },
-      order: [["name", "ASC"]],
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
 });
 
 module.exports = router;
