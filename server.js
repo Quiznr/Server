011cba52b372f8d89e012209a0c3580e6068bb4e
M4RT1N <mjlynch35@yahoo.com>
2023-07-29 22:23:26 -0500

    Added cors

--- server.js
+++ server.js
@@ -29,19 +29,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "/public")));
 
+app.use(cors());
+
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
-});
-
-// Manually set the CORS headers to allow any origin
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
 });
