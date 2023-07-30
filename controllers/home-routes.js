ac284733a0d27314510e7e5f0bf5ff2ba4eed811
M4RT1N <mjlynch35@yahoo.com>
2023-07-29 20:35:33 -0500

    updates

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const { User } = require("../models");
 
 // GET all users for homepage
-router.get("/home", async (req, res) => {
+router.get("/", async (req, res) => {
   console.log("Hello123");
 });
 
